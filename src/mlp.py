2be51fe82f9fd7052d8424d501ecdaadb18b740a
David Ryan Snyder <drsnyder@ugrad1.cs.jhu.edu>
2013-10-28 13:15:20 -0400

    working on a basic mlp

---
+++ src/mlp.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import linalg
+
+class MLP:
+  def __init__(n_input, n_hidden, n_output):
+    self.input_nodes = np.zeros(n_input )
+    self.hidden_nodes = np.zeros(n_hidden)
+    self.output_nodes = np.zeros(n_output)
+    weights = []
+    for i in range(len(self.input_nodes)):
+      weights.append(numpy.random.uniform(-1, 1, size=(len(self.hidden_nodes) + 1)))
+    self.V = np.array(weights)
+    weights = []
+    for i in range(len(self.hidden_nodes)):
+      weights.append(numpy.random.uniform(-1, 1, size=(len(self.output_nodes) + 1)))
+    self.W = np.array(weights)
+    print "V = " + str(self.V)
+    print "W = " + str(self.W)
+    
+
