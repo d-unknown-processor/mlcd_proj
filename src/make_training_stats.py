15605cc7c64f7487d6af8e58c565efae93b7dbc1
David Snyder <dsnyder@t01.clsp.jhu.edu>
2013-11-12 13:48:38 -0500

    improved script for getting training stats, added mins, maxes for rescaling

--- src/make_training_stats.py
+++ src/make_training_stats.py
@@ -5,16 +5,16 @@ root = "../../"
 sepshock_root = root + "features/sepshock/"
 sevsep_root = root + "features/sevsep/"
 sirs_root = root + "features/sirs/"
+#train_map = root + "trainset.recs"
 train_map = root + "trainset.recs"
-#{'f0': 174650769.85940018, 'f1': 18772716.698753968, 'f2': 5814838386936.324, 'f3': 6666563201149.626, 'f4': 87834758.10096925, 'f5': 39286188.27412433, 'f6': 31308445.88732576, 'f7': 27458640.814931396, 'f8': 27496105.34393987} 61453018
 
 def get_mean():
   count = 0
   sums = {'f0' : 0, 'f1': 0, 'f2' : 0, 'f3' : 0, 'f4' : 0, 'f5' : 0, 'f6' : 0, 'f7':0, 'f8' : 0}
-  sums = {'f0': 174650769.85940018, 'f1': 18772716.698753968, 'f2': 5814838386936.324, 'f3': 6666563201149.626, 'f4': 87834758.10096925, 'f5': 39286188.27412433, 'f6': 31308445.88732576, 'f7': 27458640.814931396, 'f8': 27496105.34393987} 
-  count = 61453018
+  maxs = {'f0' : None, 'f1': None, 'f2' : None, 'f3' : None, 'f4' : None, 'f5' : None, 'f6' : None, 'f7':None, 'f8' : None}
+  mins = {'f0' : None, 'f1': None, 'f2' : None, 'f3' : None, 'f4' : None, 'f5' : None, 'f6' : None, 'f7':None, 'f8' : None}
   f = open(train_map, 'r').readlines()
-  for line in f[138:]:
+  for line in f:
     sys.stderr.write(".")
     toks = line.split()
     if len(toks) < 3:
@@ -44,19 +44,27 @@ def get_mean():
     for l in data.split('\n'):
       if "Inf" in l or "NaN" in l:
         continue
-
       features = [float(x) for x in l.split()]
       if len(features) < 1:
         continue
       count += 1
       for j in range(0, len(features)):
-        sums["f" + str(j)] += features[j]
-    print sums, count
- 
-  means = []
-  for sm in sums.values():
-    means.append(sm / float(count))
-  return means, count
+        k = "f" + str(j)
+        sums[k] += features[j]
+        if maxs[k] == None:
+          maxs[k] = features[j]
+        else:
+          if maxs[k] < features[j]:
+            maxs[k] = features[j]
+        if mins[k] == None:
+          mins[k] = features[j]
+        else:
+          if mins[k] > features[j]:
+            mins[k] = features[j]
+  means = {'f0' : 0, 'f1': 0, 'f2' : 0, 'f3' : 0, 'f4' : 0, 'f5' : 0, 'f6' : 0, 'f7':0, 'f8' : 0}
+  for k in sums:
+    means[k] = (sums[k] / float(count))
+  return means, maxs, mins, count
 
 
 def get_variance(means, count):
@@ -94,18 +102,20 @@ def get_variance(means, count):
       if len(features) < 1:
         continue
       for j in range(0, len(features)):
-        squared_diffs["f" + str(j)] = (features[j] - means[j])**2
-  variances = []
-  for d in squared_diffs.values():
-    variances.append(d / float(count))
+        squared_diffs["f" + str(j)] = (features[j] - means["f" + str(j)])**2
+  variances = {'f0' : 0, 'f1': 0, 'f2' : 0, 'f3' : 0, 'f4' : 0, 'f5' : 0, 'f6' : 0, 'f7':0, 'f8' : 0}
+  for k in squared_diffs:
+    variances[k] = (squared_diffs[k] / float(count))
   return variances
     
 def main():
-  means, count = get_mean()
+  means, maxs, mins, count = get_mean()
   variances = get_variance(means, count)
   print "count = " + str(count)
   print "means = " + str(means) 
   print "var = " + str(variances)
+  print "maxs = " + str(maxs)
+  print "mins = " + str(mins)
 
 
 if __name__ == "__main__":
